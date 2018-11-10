35dbe07ae1392d4067363fba09356075cf4f587c
Amee Covarrubias <ameecova@yahoo.com>
2018-11-10 07:47:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,7 +46,7 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "Write your SQL query here"
   "SELECT characters.name, COUNT(character_books.book_id)
-  FROM characters
+  FROM character_books
   LEFT JOIN characters ON character_books.book_id = characters.id 
   GROUP BY characters.name
   ORDER BY COUNT(character_books.book_id) DESC"
